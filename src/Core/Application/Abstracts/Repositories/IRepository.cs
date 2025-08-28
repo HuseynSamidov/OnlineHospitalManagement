cda78adf10e27b9ec024c0e5ac0593d6b74c74b7
Huseyn <samidovhuseyn@gmail.com>
2025-08-28 21:12:20 +0400

    Repository added

---
+++ src/Core/Application/Abstracts/Repositories/IRepository.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstracts.Repositories;
+
+public interface IRepository<T> where T : BaseEntity
+{
+    //Read IRepository
+    Task<T?> GetByIdAsync(Guid id);
+    IQueryable<T> GetAll(bool isTracking = false);
+
+
+    IQueryable<T> GetByFiltered(Expression<Func<T, bool>> predicate,
+                         Expression<Func<T, object>>[]? include = null,
+                         bool isTracking = false);
+    IQueryable<T> GetAllFiltered(Expression<Func<T, bool>> predicate,
+                         Expression<Func<T, object>>[]? include = null,
+                         Expression<Func<T, object>>? orderby = null,
+                         bool isOrderByAsc = true,
+                         bool isTracking = false);
+    //Write IRepository
+    Task AddAsync(T entity);
+    void Delete(T entity);
+    void Update(T entity);
+    Task SaveChangeAsync();
+}
