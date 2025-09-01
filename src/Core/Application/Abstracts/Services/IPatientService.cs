0b8405029c90924e318221080238127d3811cd0b
Huseyn <samidovhuseyn@gmail.com>
2025-09-01 23:34:31 +0400

    idk

---
+++ src/Core/Application/Abstracts/Services/IPatientService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstracts.Services;
+
+public interface IPatientService
+{
+    Task<Patient?> GetByIdAsync(Guid id);
+    Task<List<Patient>> GetAllAsync();  
+    Task<Patient> CreateAsync(Patient patient);
+    Task<Patient> UpdateAsync(Patient patient);
+    Task<bool> DeleteAsync(Guid id);
+
+    Task<Patient?> GetPatientWithTicketsAsync(Guid id);
+    Task<bool> HasActiveTicketAsync(Guid patientId);
+}
