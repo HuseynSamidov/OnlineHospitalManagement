7c5d42bb9ec3d97dcf354112aa458126f028c629
Huseyn <samidovhuseyn@gmail.com>
2025-09-02 20:14:23 +0400

    Abstract Services completed

---
+++ src/Core/Application/Abstracts/Services/IDoctorService.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstracts.Services;
+
+public interface IDoctorService
+{
+    // CRUD metodları
+    Task<Doctor?> GetByIdAsync(Guid id);
+    Task<List<Doctor>> GetAllAsync();
+    Task<Doctor> CreateAsync(Doctor doctor);
+    Task<Doctor> UpdateAsync(Doctor doctor);
+    Task<bool> DeleteAsync(Guid id);
+    /// Verilmiş department-ə aid olan bütün həkimləri gətirir
+    Task<List<Doctor>> GetDoctorsByDepartmentAsync(Guid departmentId);
+
+    /// Müəyyən tibbi xidmət üzrə çalışan həkimləri gətirir
+    Task<List<Doctor>> GetDoctorsByServiceAsync(Guid serviceId);
+}
---
+++ src/Core/Application/Abstracts/Services/IQueueTicketService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstracts.Services;
+
+public interface IQueueTicketService
+{
+    Task<QueueTicket> CreateAsync(Guid patientId, Guid medicalServiceId);
+
+    Task<bool> UpdateStatusAsync(Guid ticketId, QueueStatus newStatus);
+    Task ProcessMissedTicketAsync(Guid ticketId);
+    Task<IEnumerable<QueueTicket>> GetByPatientIdAsync(Guid patientId);
+
+    Task<IEnumerable<QueueTicket>> GetByMedicalServiceIdAsync(Guid medicalServiceId);
+
+    // Aktiv ticket-lər (Waiting və Called olanlar)
+    Task<IEnumerable<QueueTicket>> GetActiveTicketsAsync();
+    Task<QueueTicket?> GetByIdAsync(Guid id);
+}
