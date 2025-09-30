17484133b8f8aaaeacc780dd27f82b8d478e4aaf
Huseyn <samidovhuseyn@gmail.com>
2025-09-30 22:51:14 +0400

    endpoints added

--- src/Presentation/HospitalAPI/Controllers/CategoryController.cs
+++ src/Presentation/HospitalAPI/Controllers/CategoryController.cs
@@ -1,5 +1,9 @@
 ﻿using Application.Abstracts.Services;
 using Application.DTOs.CategoryDTOs.Create;
+using Application.DTOs.CategoryDTOs.Delete;
+using Application.DTOs.CategoryDTOs.Update;
+using Application.Shared;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HospitalAPI.Controllers;
@@ -16,6 +20,7 @@ public class CategoryController : ControllerBase
     }
 
     [HttpPost]
+    [Authorize(Policy = Permissions.Category.MainCreate)]
     public async Task<IActionResult> CreateDepartment([FromBody] CreateDepartmentDto dto)
     {
         var result = await _categoryService.CreateDepartmentCategory(dto);
@@ -23,9 +28,26 @@ public class CategoryController : ControllerBase
     }
 
     [HttpPost]
+    [Authorize(Policy = Permissions.Category.SubCreate)]
     public async Task<IActionResult> CreateProcedure([FromBody] UpdateProcedureDto dto)
     {
         var result = await _categoryService.CreateProcedureCategory(dto);
+        return StatusCode((int)result.StatusCode, result);
+    }
+
+    [HttpPut]
+    [Authorize(Policy = Permissions.Category.MainUpdate)]
+    public async Task<IActionResult> UpdateDepartment([FromBody] UpdateDepartmentDto dto)
+    {
+        var result = await _categoryService.UpdateDepartmentAsync(dto);
+        return StatusCode((int)result.StatusCode, result);
+    }
+
+    [HttpDelete]
+    [Authorize(Policy = Permissions.Category.Delete)]
+    public async Task<IActionResult> DeleteDepartment([FromBody] DeleteDepartmentDto dto)
+    {
+        var result = await _categoryService.DeleteDepartmentAsync(dto);
         return StatusCode((int)result.StatusCode, result);
     }
 
